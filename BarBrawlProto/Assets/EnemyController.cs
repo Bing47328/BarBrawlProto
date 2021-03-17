b9682b683432347b57ac4004277eb219ddac0a09
Mario Egocheaga <work.mario.egocheaga@gmail.com>
2021-03-16 22:59:33 -0400

    Unity

--- BarBrawlProto/Assets/EnemyController.cs
+++ BarBrawlProto/Assets/EnemyController.cs
@@ -6,6 +6,14 @@ public class EnemyController : MonoBehaviour
 {
     public int health;
 
+    public GameObject deathAnim;
+
+    public float playerRange = 10f;
+
+    public Rigidbody2D rb;
+    public float moveSpeed;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +24,12 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Distance(transform.position, PlayerController.instance.transform.position) < playerRange)
+        {
+            Vector3 playerDirection = PlayerController.instance.transform.position - transform.position;
+
+            rb.velocity = playerDirection.normalized * moveSpeed;
+        }
     }
 
     public void TakeDMG()
@@ -25,6 +38,7 @@ public class EnemyController : MonoBehaviour
         if (health <= 0)
         {
             Destroy(gameObject);
+            Instantiate(deathAnim, transform.position, transform.rotation);
         }
     }
 }
